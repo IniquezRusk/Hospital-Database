735ed0d0238a8d5cffc5cf1914584fa0661b1c91
Adewole <148657016+GreatHeavens@users.noreply.github.com>
2025-11-19 16:21:45 -0600

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();        // Resume the session
-session_unset();        // Remove all session variables
-session_destroy();      // Destroy the session
-header('Location: login.php');  // Redirect to login page
-exit;
-?>
